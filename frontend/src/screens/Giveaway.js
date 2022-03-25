74b5fdbe7feb69b27ccf7f8c97b388ce8092effa
MutahirPluto <96816612+MutahirPluto@users.noreply.github.com>
2022-03-25 20:40:30 +0500

    Updated

--- frontend/src/screens/Giveaway.js
+++ frontend/src/screens/Giveaway.js
@@ -1,6 +1,20 @@
 import { Col, Container, Row, Form, Button } from "react-bootstrap";
+import Web3Modal from "web3modal";
+import {ethers, BigNumber} from "ethers";
+
 
 function Giveaway(props){
+
+    const loadProvider = async () => {
+        try {
+          const web3Modal = new Web3Modal();
+          const connection = await web3Modal.connect();
+          const provider = new ethers.providers.Web3Provider(connection);
+          return provider.getSigner();
+        } catch (e) {
+          console.log("loadProvider default: ", e);
+        }
+      };
 
     return(
 
